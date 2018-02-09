95020a8be0c780f5ee8c7fbfe8b8dbcc20fdac15
HeejoSuh <15654639+HeejoSuh@users.noreply.github.com>
2018-02-09 12:47:42 -0500

    Add files via upload

---
+++ helloWorld.swift
@@ -0,0 +1,15 @@
+//
+
+//  HelloWorld.swift
+
+//  This program prints out Hello World
+
+//
+
+//  Created by Heejo Suh in Feb 2018.
+
+//  Copyright © 2018 MTHS. All rights reserved.
+
+//
+
+print("Hello World!");
